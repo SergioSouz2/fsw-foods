a4e7c14236568b1e0f03559ad7f229660f634821
SergioSouz2 <sergio.tbl0123@gmail.com>
2024-05-01 16:51:50 -0300

    :sparkles: feat: add promo banner to home

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image"
 import CategoryList from "./_components/category-list"
 import Header from "./_components/header"
 import Search from "./_components/search"
@@ -6,12 +7,27 @@ const Home = () => {
    return (
       <>
          <Header/>
+
          <div className="px-5 pt-6">
             <Search/>
          </div>
+
          <div className="px-5 pt-6">
             <CategoryList/>
          </div>
+
+         <div className="px-5 pt-6">
+            <Image  
+               height={0}
+               width={0}
+               src="/promo-banner-01.png" 
+               alt="Até 30% de desconto em pizzas"
+               className="w-full h-auto object-contain"
+               sizes="100vw"
+               quality={100}
+            />
+         </div>
+   
       </>
    )
 }
