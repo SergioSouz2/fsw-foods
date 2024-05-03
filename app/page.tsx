db749a3634b53dac20ee95b99b0543167706dd00
SergioSouz2 <sergio.tbl0123@gmail.com>
2024-05-03 14:52:08 -0300

    :sparkles: feat: add recommended products page

--- app/page.tsx
+++ app/page.tsx
@@ -54,10 +54,12 @@ const Home = async () => {
          <div className="pt-6 space-y-4">
             <div className="px-5 flex justify-between items-center" >
                <h2 className="font-semibold">Pedidos Recomendados</h2>
+            <Link href={`/products/recommended`}>
                <Button variant={"ghost"} className="h-fit text-primary p-0 hover:bg-transparent hover:text-primary">
                   Ver todos
                   <ChevronRightIcon size={16}/>
                </Button>
+            </Link>
             </div>
 
             <ProductList products={products}/>
